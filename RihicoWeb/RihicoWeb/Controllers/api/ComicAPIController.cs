18862f6d923eb99af256d8a1150b62cafd7b31bf
Cayden <xuebaibai2@hotmail.com>
2017-04-22 20:00:43 +1000

    Extra setting completed
    Complete angular js frontend setting with $resource and umbraco webapi

---
+++ RihicoWeb/RihicoWeb/Controllers/api/ComicAPIController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace RihicoWeb.Controllers.api
+{
+    [PluginController("webAPI")]
+    public class ComicAPIController : MasterAPIController
+    {
+        [HttpGet]
+        public IEnumerable<string> GetAllComic()
+        {
+            return new[] { "One Piece", "Naruto", "Bleech" };
+        }
+    }
+}
---
+++ RihicoWeb/RihicoWeb/Scripts/frontend/getcomic.js
@@ -0,0 +1,29 @@
+﻿var app = angular.module('getComic', ['ngResource']);
+app.controller('getComicController', function ($scope, getComicResource) {
+    $scope.model = {};
+    $scope.users = getComicResource.getUser.query();
+    $scope.comics = getComicResource.getComics.query();
+    console.log(getComicResource.getAnotherComics.query());
+    $scope.getComic = function () {
+
+    };
+});
+app.factory("getComicResource", function ($resource) {
+    return {
+        getUser: $resource('https://jsonplaceholder.typicode.com/users/:user', { user: '@user' }, {
+            update: {
+                method: 'PUT'
+            }
+        }),
+        getComics: $resource('/Umbraco/webAPI/ComicAPI/GetAllComic'),
+        getAnotherComics: $resource('/api/NotUmbracoAPI/GetAllComic')
+    }
+})
+//return {
+//    states: $resource('../data/states.json', {}, {
+//        query: { method: 'GET', params: {}, isArray: false }
+//    }),
+//    countries: $resource('../data/countries.json', {}, {
+//        query: { method: 'GET', params: {}, isArray: false }
+//    })
+//};
