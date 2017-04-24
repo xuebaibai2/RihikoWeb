66077dec7a5ba46a3c2ec1b134e96a2a3ca73c9f
Cayden <xuebaibai2@hotmail.com>
2017-04-25 04:29:12 +1000

    Able to save images to umbraco media

--- RihicoWeb/RihicoWeb/Controllers/api/ComicAPIController.cs
+++ RihicoWeb/RihicoWeb/Controllers/api/ComicAPIController.cs
@@ -11,7 +11,9 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Script.Serialization;
 using Umbraco.Web.Mvc;
+using System.IO.Compression;
 using Umbraco.Web.WebApi;
+using Umbraco.Core.Models;
 
 namespace RihicoWeb.Controllers.api
 {
@@ -41,20 +43,15 @@ namespace RihicoWeb.Controllers.api
             }
         }
 
-        public async Task GetDownloadImgAsync(string imgSrcListJson)
+        public async System.Threading.Tasks.Task<dynamic> GetDownloadImgAsync(string imgSrcListJson)
         {
             var comicList = new JavaScriptSerializer().Deserialize<List<Comic>>(imgSrcListJson);
             var fileName = Guid.NewGuid().ToString();
             ComicFolder comicFolder = new ComicFolder(comicList, fileName);
-            var dirInfo = System.IO.Directory.CreateDirectory(Path.GetTempPath() + "comic/" + comicFolder.TempFolderName);
 
             var ms = Services.MediaService;
-            var cs = Services.ContentService;
-            //var mediaMap = ms.CreateMedia(comicFolder.TempFolderName, 1070, "Folder");
-            //string result = Path.GetTempPath();
-            //Use the MediaService to Save the new Media object
-            //ms.Save(mediaMap);
-
+            var mediaMap = ms.CreateMedia(comicFolder.Comics[0].Title, 1070, "Folder");
+            ms.Save(mediaMap);
             using (WebClient wc = new WebClient())
             {
                 wc.Headers.Add("User-Agent", CONSTVALUE.USER_AGENT);
@@ -63,17 +60,46 @@ namespace RihicoWeb.Controllers.api
                 {
                     foreach (Comic comic in comicFolder.Comics)
                     {
-                        await wc.DownloadFileTaskAsync(comic.Url, Path.GetTempPath() + "comic/" + comicFolder.TempFolderName + "\\" + comic.Page + ".jpg");
-                        //var tempImg = ms.CreateMedia(comic.Page, mediaMap, "Image");
-                        //tempImg.SetValue("umbracoBytes", test);
+                        try
+                        {
+                            var tempImg = await wc.OpenReadTaskAsync(comic.Url);
+
+                            using (var memStream = new MemoryStream())
+                            {
+                                tempImg.CopyTo(memStream);
+                                tempImg.Dispose();
+                                var mediaImage = ms.CreateMedia(comic.Page, mediaMap, "Image");
+                                mediaImage.SetValue("umbracoFile", comic.Page + ".jpg", memStream);
+                                ms.Save(mediaImage);
+                            }
+                        }
+                        catch (WebException wEx)
+                        {
+                            HttpWebResponse webResponse = (HttpWebResponse)wEx.Response;
+                            if (webResponse.StatusCode == HttpStatusCode.NotFound)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                return wEx;
+                            }
+                        }
+                        
                     }
+                    return mediaMap.Id;
                 }
                 catch (Exception ex)
                 {
-                    throw ex;
-                    //return ex;
+                    //throw ex;
+                    return ex;
                 }
             }
+        }
+
+        private bool IsDownloadCompleted(ComicFolder comicFolder, string dirPath)
+        {
+            return comicFolder.Comics.Count == Directory.GetFiles(dirPath).Count();
         }
     }
 }
