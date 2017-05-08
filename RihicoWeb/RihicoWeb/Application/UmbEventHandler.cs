fe06b9a5708ef4fde6a7ea5589da85d5af6f76bc
Cayden <caydenli@outlook.com>
2017-05-08 14:52:43 +1000

    update UmbEventHandler

--- RihicoWeb/RihicoWeb/Application/UmbEventHandler.cs
+++ RihicoWeb/RihicoWeb/Application/UmbEventHandler.cs
@@ -19,12 +19,27 @@ namespace RihicoWeb.Application
             ServerVariablesParser.Parsing += SubscribeServerVariable;
         }
 
-        private void SubscribeServerVariable(object sender, Dictionary<string, object> umbracoUrls)
+        private void SubscribeServerVariable(object sender, Dictionary<string, object> serverVars)
         {
+            if (!serverVars.ContainsKey("umbracoUrls"))
+                throw new Exception("Missing umbracoUrls.");
+            var umbracoUrlsObject = serverVars["umbracoUrls"];
+            if (umbracoUrlsObject == null)
+                throw new Exception("Null umbracoUrls");
+            var umbracoUrls = umbracoUrlsObject as Dictionary<string, object>;
+            if (umbracoUrls == null)
+                throw new Exception("Invalid umbracoUrls");
+
+            if (!serverVars.ContainsKey("umbracoPlugins"))
+                throw new Exception("Missing umbracoPlugins.");
+            var umbracoPlugins = serverVars["umbracoPlugins"] as Dictionary<string, object>;
+            if (umbracoPlugins == null)
+                throw new Exception("Invalid umbracoPlugins");
+
             if (HttpContext.Current == null) throw new InvalidOperationException("HttpContext is null");
             var urlHelper = new UrlHelper(new RequestContext(new HttpContextWrapper(HttpContext.Current), new RouteData()));
 
-            (umbracoUrls["umbracoUrls"] as Dictionary<string, object>)["comicSyncTool"] = urlHelper.GetUmbracoApiServiceBaseUrl<ComicSyncToolController>(x => x.Init());
+            umbracoUrls.Add("comicSyncTool", urlHelper.GetUmbracoApiServiceBaseUrl<ComicSyncToolController>(x => x.Init()));
         }
     }
 }
